59baa78e8b060056251d2b8b4dc3d005a1e42c9c
Dr Jan-Jan van der Vyver ⅩⅣ <jan.jan.code@gmail.com>
2019-12-15 15:14:00 -0800

    seems to work

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
 `use strict`
 
 const toml = require('toml')
-const fs = require('fs')
+const fs = require('fs').promises
 const { promisify } = require('util')
 
 const defaults = require('./lib/defaults')
 const graphify = require('./lib/graphify')
-//const output = require('./lib/output')
-
-
-const readFileAsync = promisify(fs.readFile)
-
-
-const writeFileAsync = promisify(fs.writeFile)
-
-
-const throwError = errMsg => {
-  throw new Error(chalk.red.bold(errMsg))
-}
+const output = require('./lib/output')
 
 
 const run = async () => {
   try {
-    const res = (await readFileAsync('plan.toml')).toString()
-    const results = graphify(defaults(toml.parse(res)))
-  
+    const res = (await fs.readFile('plan.toml')).toString()
+    const results = output(graphify(defaults(toml.parse(res))))
+    await fs.writeFile(`plan.html`, results)
   } catch (err) {
     console.error(err)
   }
