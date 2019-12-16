91183baabe5f17c62b1f22900b1c9af6e2eb410e
Dr Jan-Jan van der Vyver ⅩⅣ <jan.jan.code@gmail.com>
2019-12-16 16:35:18 -0500

    yargs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 `use strict`
 
 const toml = require('toml')
@@ -7,13 +8,35 @@ const { promisify } = require('util')
 const defaults = require('./lib/defaults')
 const graphify = require('./lib/graphify')
 const output = require('./lib/output')
+const {
+  arrayify,
+} = require('./lib/utils')
 
+const argv = require('yargs')
+  .usage('Usage: $0 -i [file1] [file2]')
+  .alias('i', 'input')
+  .default('i', ['plan'])
+  .describe('i', 'load input from specified files (the ".toml" extension will be added)')
+  .help('h')
+  .alias('h', 'help')
+  .argv;
+
+
+const read = async files => {
+  let config = {}
+  for (const file of files) {
+    const input = toml.parse((await fs.readFile(`${file}.toml`)).toString())
+    config = { ...config, ...input }
+  }
+  return config
+}
 
 const run = async () => {
   try {
-    const res = (await fs.readFile('plan.toml')).toString()
-    const results = output(graphify(defaults(toml.parse(res))))
-    await fs.writeFile(`plan.html`, results)
+    const files = arrayify(argv.i)
+    const config = await read(files)
+    const results = output(graphify(defaults(config)))
+    await fs.writeFile(`${files.join('-')}.html`, results)
   } catch (err) {
     console.error(err)
   }
